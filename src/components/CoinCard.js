9ae9b3c403441d65b8c2dc4d640d53534ca57c78
Leonardo Mendes <leo.mi.me@gmail.com>
2017-12-28 12:59:19 -0200

    Initial Coin Card

---
+++ src/components/CoinCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+import {images} from '../Utils/CoinIcons';
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+    },
+    image: {
+        width: 40,
+        height: 40,
+    },
+    bold: {
+        fontWeight: 'bold',
+    },
+});
+
+const { container, image, bold } = styles;
+
+export default CoinCard = ({ symbol, coin_name, price_usd, percent_change_24h, percent_change_7d }) => (
+    <View style={container}>
+        <Image 
+            styles={styles.image}
+            source={{uri: images[symbol]}}
+        />
+        <Text>{symbol}</Text>
+        <Text>{coin_name}</Text>
+        <Text>{price_usd} <Text style={bold}></Text></Text>
+        <Text>Change past 24 hours: {percent_change_24h}</Text>
+        <Text>Change past 7 days: {percent_change_7d}</Text>
+    </View>
+);
