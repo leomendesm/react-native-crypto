139ebb9cf1cc517d50143b0151f30d01836969ec
Leonardo Mendes <leo.mi.me@gmail.com>
2017-12-28 12:59:04 -0200

    Windows bug fix

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -3,16 +3,22 @@ import { connect } from 'react-redux';
 import {View, Text} from 'react-native';
 
 import FetchCoinData from './../Actions/FetchCoinData';
+import CoinCard from './CoinCard';
 
 class CryptoContainer extends Component {
     componentDidMount(){
         this.props.FetchCoinData();
     }
+    renderCoinCards() {
+        const { crypto } = this.props;
+        console.log(crypto)
+    }
     render() {
+        const { crypto } = this.props;        
         return (
             <View>
                 <Text>
-                    Container
+                    {this.renderCoinCards()}
                 </Text>
             </View>
         )
