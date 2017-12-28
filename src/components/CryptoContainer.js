3e2a832717a43d7811271dbd3f7cdf48d754da69
Leonardo Mendes <leo.mi.me@gmail.com>
2017-12-28 13:08:46 -0200

    CoinCard Finished and spinner loader added

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {View, Text} from 'react-native';
 
+import Spinner from 'react-native-loading-spinner-overlay';
+
 import FetchCoinData from './../Actions/FetchCoinData';
 import CoinCard from './CoinCard';
 
@@ -12,9 +14,32 @@ class CryptoContainer extends Component {
     renderCoinCards() {
         const { crypto } = this.props;
         console.log(crypto)
+        return crypto.data.map((coin, index) => 
+            <CoinCard 
+                key={index}
+                coin_name={coin.name}
+                symbol={coin.symbol}
+                price_usd={coin.price_usd}
+                percent_change_24h={coin.percent_change_24h}
+                percent_change_7d={coin.percent_change_7d}
+            />
+        )
     }
     render() {
-        const { crypto } = this.props;        
+        const { crypto } = this.props;
+        if(crypto.isFetching){
+            return (
+                <View>
+                    <Spinner
+                        visible={crypto.isFetching}
+                        textContent={'Loading...'}
+                        textStyle={{color: '#253145'}}
+                        animation='fade'
+                    />
+                </View>
+            )
+        }        
+        
         return (
             <View>
                 <Text>
